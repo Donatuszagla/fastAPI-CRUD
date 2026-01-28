1f58cfeee2145175fcfa53be6d8f4d1a033ba693
Donatus Zagla <beneesolomon765@gmail.com>
2026-01-28 15:52:14 +0000

    add testing for all routes including protected routes

--- test_main.py
+++ test_main.py
@@ -1,8 +1,9 @@
+from users import create_access_token
 import pytest
+import pytest_asyncio
 from db import engine
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
-import pytest
 from httpx import ASGITransport, AsyncClient
 from main import app
 from models import User, Todo, Base
@@ -15,7 +16,7 @@ def test_client():
     return client
 
 @pytest.fixture
-def seed_data():
+def user():
     session = Session(engine)
     user = User(
         id=1,
@@ -24,45 +25,65 @@ def seed_data():
         password_hashed=get_password_hash("test_password"),
         is_active=True
     )
+    session.add(user)
+    session.commit()
+    yield user
+    session.query(User).delete()
+    session.commit()
 
+@pytest.fixture
+def user_authorization_header(user):
+    token = create_access_token(data={"sub": user.email})
+    return {
+        "Authorization": f"Bearer {token}"
+    }
+
+@pytest.fixture
+def todo():
+    session = Session(engine)
     todo = Todo(
         id=1,
-        title="Seed Todo",
-        description="Seed Description",
+        title="Test Todo",
+        description="Test Description",
         completed=False,
         priority="Low"
     )
-
-    session.add(user)
     session.add(todo)
     session.commit()
+    yield todo
+    session.query(Todo).delete()
+    session.commit()    
 
-    yield session   
-
-    session.query(User).delete()
+@pytest.fixture
+def todos():
+    session = Session(engine)
+    todos = [
+        Todo(
+            id=2,
+            title="Test Todo 2",
+            description="Test Description 2",
+            completed=False,
+            priority="Low"
+        ),
+        Todo(
+            id=3,
+            title="Test Todo 3",
+            description="Test Description 3",
+            completed=False,
+            priority="Low"
+        )
+    ]
+    session.add_all(todos)
+    session.commit()
+    yield todos
     session.query(Todo).delete()
     session.commit()
 
 @pytest.fixture
-def fake_context():
-    async def fake_get_context(request):
-        return {
-            "request": request,
-            "user": {
-                "id": 1,
-                "email": "test_user@example.com",
-                "is_active": True,
-            }
-        }
+def seed_data(user, todo, todos):
+    session = Session(engine)
+    yield session   
 
-    from main import graphql_app
-
-    old_context_getter = graphql_app.context_getter
-    graphql_app.context_getter = fake_get_context
-
-    yield 
-
-    graphql_app.context_getter = old_context_getter
 
 
 # def test_todos(test_client, seed_data):
@@ -292,8 +313,17 @@ def fake_context():
 #     assert response.json()["data"]["loginForAccessToken"] is not None
 
 
-@pytest.mark.anyio
-async def test_todos(seed_data):
+@pytest_asyncio.fixture
+async def client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://",
+    ) as ac:
+        yield ac
+
+
+
+
+async def test_todos(client, seed_data):
     query = """
         query {
             todos {
@@ -305,23 +335,34 @@ async def test_todos(seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json().get("data").get("todos") == [
         {
             "id": 1,
-            "title": "Seed Todo",
-            "description": "Seed Description",
+            "title": "Test Todo",
+            "description": "Test Description",
+            "completed": False,
+            "priority": "Low"
+        },
+        {
+            "id": 2,
+            "title": "Test Todo 2",
+            "description": "Test Description 2",
+            "completed": False,
+            "priority": "Low"
+        },
+        {
+            "id": 3,
+            "title": "Test Todo 3",
+            "description": "Test Description 3",
             "completed": False,
             "priority": "Low"
         }
     ]  
 
-@pytest.mark.anyio
-async def test_todo(seed_data):
+
+async def test_todo(client, seed_data):
     query = """
         query {
             todo(
@@ -335,88 +376,78 @@ async def test_todo(seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["todo"] == {
         "id": 1,
-        "title": "Seed Todo",
-        "description": "Seed Description",
+        "title": "Test Todo",
+        "description": "Test Description",
         "completed": False,
         "priority": "Low"
     }  
     
 
-# def test_create_todo(test_client, seed_data, fake_context):
-#     query = """
-#         mutation {
-#             createTodo(
-#                 title: "Test Todo"
-#                 description: "Test Description"
-#                 completed: false
-#                 priority: "Low"
-#             ) {
-#                 id
-#                 title
-#                 description
-#                 completed
-#                 priority
-#             }
-#         }
-#     """
-#     headers = {
-#         "Authorization": "Bearer fake_token"
-#     }
-#     response = test_client.post("/graphql", json={"query": query}, headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["data"]["createTodo"] is not None
+async def test_create_todo(client,user_authorization_header,seed_data):
+    query = """
+        mutation {
+            createTodo(
+                title: "Test Todo"
+                description: "Test Description"
+                completed: false
+                priority: "Low"
+            ) {
+                id
+                title
+                description
+                completed
+                priority
+            }
+        }
+    """
+    response = await client.post("/graphql", json={"query": query}, headers=user_authorization_header)
+    assert response.status_code == 200
+    assert response.json()["data"]["createTodo"] is not None
 
-# def test_update_todo(test_client):
-#     query = """
-#         mutation {
-#             updateTodo(
-#                 todoId: 1
-#                 title: "Updated Todo"
-#                 description: "Updated Description"
-#                 completed: true
-#                 priority: "High"
-#             ) {
-#                 id
-#                 title
-#                 description
-#                 completed
-#                 priority
-#             }
-#         }
-#     """
-#     headers = {
-#         "Authorization": "Bearer fake_token"
-#     }
-#     response = test_client.post("/graphql", json={"query": query}, headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["data"]["updateTodo"] is not None    
 
-# def test_delete_todo(test_client):
-#     query = """
-#         mutation {
-#             deleteTodo(
-#                 todoId: 1
-#             ) {
-#                 id
-#             }
-#         }
-#     """
-#     headers = {
-#         "Authorization": "Bearer fake_token"
-#     }
-#     response = test_client.post("/graphql", json={"query": query}, headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["data"]["deleteTodo"] is not None
+async def test_update_todo(client,user_authorization_header,seed_data):
+    query = """
+        mutation {
+            updateTodo(
+                todoId: 1
+                title: "Updated Todo"
+                description: "Updated Description"
+                completed: true
+                priority: "High"
+            ) {
+                id
+                title
+                description
+                completed
+                priority
+            }
+        }
+    """
+    response = await client.post("/graphql", json={"query": query}, headers=user_authorization_header)
+    assert response.status_code == 200
+    assert response.json()["data"]["updateTodo"] is not None    
 
-@pytest.mark.aanyio
-async def test_get_users(seed_data):
+
+async def test_delete_todo(client,user_authorization_header,seed_data):
+    query = """
+        mutation {
+            deleteTodo(
+                todoId: 1
+            ) {
+                id
+            }
+        }
+    """
+    response = await client.post("/graphql", json={"query": query}, headers=user_authorization_header)
+    assert response.status_code == 200
+    assert response.json()["data"]["deleteTodo"] is not None
+
+
+async def test_get_users(client, seed_data):
     query = """
         query {
             getUsers {
@@ -427,15 +458,12 @@ async def test_get_users(seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["getUsers"] is not None 
 
-@pytest.mark.aanyio
-async def test_get_user( seed_data):
+
+async def test_get_user(client, seed_data):
     query = """
         query {
             getUser(
@@ -447,10 +475,7 @@ async def test_get_user( seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["getUser"] == {
         "id": 1,
@@ -458,8 +483,8 @@ async def test_get_user( seed_data):
         "email": "test_user@example.com",
     }
 
-@pytest.mark.aanyio
-async def test_create_user( seed_data):
+
+async def test_create_user(client, seed_data):
     query = """
         mutation {
             createUser(
@@ -475,15 +500,12 @@ async def test_create_user( seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["createUser"] is not None   
 
-@pytest.mark.aanyio
-async def test_update_user( seed_data):
+
+async def test_update_user(client, seed_data):
     query = """
         mutation {
             updateUser(
@@ -499,15 +521,12 @@ async def test_update_user( seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["updateUser"] is not None   
 
-@pytest.mark.aanyio
-async def test_delete_user( seed_data):
+
+async def test_delete_user(client, seed_data):
     query = """
         mutation {
             deleteUser(
@@ -517,15 +536,12 @@ async def test_delete_user( seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["deleteUser"] is not None   
 
-@pytest.mark.aanyio
-async def test_login_for_access_token( seed_data):
+
+async def test_login_for_access_token(client, seed_data):
     query = """
         mutation {
             loginForAccessToken(
@@ -537,10 +553,7 @@ async def test_login_for_access_token( seed_data):
             }
         }
     """
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.post("/graphql", json={"query": query})
+    response = await client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["loginForAccessToken"] is not None
 
